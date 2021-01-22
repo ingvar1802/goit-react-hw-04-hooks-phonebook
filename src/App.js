de806db3e2ace8460e9bca4da17806ea7722d0d5
Ihor Nadieien <ingwar1802@gmail.com>
2021-01-22 20:55:45 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ import ContactList from './components/ContactList';
 
  
 const savedContacts = [
-  { id: 'id-1', name: 'Elon Mask', number: '10664888778' },
-  { id: 'id-2', name: 'Lena Kharchenko', number: '380664969795' },
-  { id: 'id-3', name: 'Bill Gates', number: '10662475771' },
-  { id: 'id-4', name: 'Mark Zuckerberg ', number: '10625884318' },
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
 ];
 
 function App() {
